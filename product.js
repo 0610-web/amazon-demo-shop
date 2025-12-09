9f2f9427bfbf86bfa6e2b0f0faa5f51cd2a2f483
0610-web <3337229223@qq.com>
2025-12-09 18:50:34 +0800

    Update product.js

--- product.js
+++ product.js
@@ -1,39 +1,16 @@
-function getQuery(name) {
-  const url = new URL(window.location.href);
-  return url.searchParams.get(name);
-}
-
-const id = getQuery("id");
-
 fetch("products.json")
-  .then(res => res.json())
-  .then(products => {
-    const product = products.find(p => p.id == id);
+    .then(res => res.json())
+    .then(data => {
+        let html = "";
+        data.forEach(item => {
+            html += `
+            <div class="product-card">
+                <img src="${item.image}">
+                <div class="product-name">${item.name}</div>
+                <div class="price">$${item.price}</div>
+                <div class="add-cart">Add to Cart</div>
+            </div>`;
+        });
 
-    document.getElementById("product-container").innerHTML = `
-      <div class="product-container">
-        
-        <div class="product-images">
-          <img src="${product.images[0]}" class="main-image">
-        </div>
-
-        <div class="product-info">
-          <h2>${product.title}</h2>
-          <p class="price">$${product.price}</p>
-          <p>${product.description}</p>
-        </div>
-
-        <div class="product-buybox">
-          <button class="addcart-btn" onclick="addToCart(${product.id})">Add to Cart</button>
-          <button class="buy-btn" onclick="window.location='thankyou.html'">Buy Now</button>
-        </div>
-      </div>
-    `;
-  });
-
-function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart") || "[]");
-  cart.push(id);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to cart!");
-}
+        document.getElementById("product-list").innerHTML = html;
+    });
